959bd112a026868384adfcc6040b7b6b6f309bfb
RC_Student_lab <RC_Student_lab@10.0.0.18>
2024-10-10 09:49:01 +0000

    Created a new class with 4 methods, and a while-loop with switch-case statements

---
+++ src/main/java/com/mycompany/icetask9/Calculations.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.icetask9;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+class Calculations {
+  
+    
+    public double addition(double result,double number1, double number2){
+            result = number1 + number2;
+            return result;
+        }
+    public double subtraction(double result,double number1, double number2){
+         result = number1 - number2;
+            return result;
+        }
+    public double division(double result,double number1, double number2){
+      result = number1 / number2;
+            return result;
+        }
+    public double multiplication(double result,double number1, double number2){
+      result = number1 * number2;
+            return result;
+        }
+}
--- src/main/java/com/mycompany/icetask9/ICETASK9.java
+++ src/main/java/com/mycompany/icetask9/ICETASK9.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.icetask9;
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane; /* Or import javax.swing.*;
 
 /**
  *
@@ -12,10 +12,16 @@ import javax.swing.JOptionPane;
 public class ICETASK9 {
 
     public static void main(String[] args) {
+        
+        //Created an instance of a class
+        Calculations Calculations = new Calculations();
+        
         //Declarations
+        boolean running =true;
         String value1;
         String value2;
-        String option;
+        int option = 0;
+        
         
         //Prompt user for 2 numbers
         value1 = JOptionPane.showInputDialog("Enter the first number:");
@@ -24,6 +30,39 @@ public class ICETASK9 {
         value2 = JOptionPane.showInputDialog("Enter the second number:");
         double number2 =Double.parseDouble(value2);
         
+        while( running){
+        String menu = "Select an operation option to perform:\n"
+                        + "1. Addition\n"
+                        + "2. Subtraction\n"
+                        + "3. Division\n"
+                        + "4. Multiplication\n"
+                        + "0. Exit";
+        String inputOption = JOptionPane.showInputDialog(menu);
+        option = Integer.parseInt(inputOption);
         
+        double result = 0;
+  
+            switch (option) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, Calculations.addition(result, number1, number2));
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null,Calculations.subtraction(result, number1, number2));
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null,Calculations.division(result, number1, number2));
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null,Calculations.multiplication(result, number1, number2));
+                    break;
+                case 0:
+                    running = false;
+                    JOptionPane.showMessageDialog(null, "Exiting application.");
+                    break;
+                default: 
+                    JOptionPane.showMessageDialog(null,"Option not recognized");
+                }
+            }
+        }
     }
-}
+
