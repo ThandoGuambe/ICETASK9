b7e6d162f1af5b8ce6be29a4d5c3674f54b4ecf1
RC_Student_lab <RC_Student_lab@10.0.0.18>
2024-10-10 06:42:06 +0000

    Imported JOptionPane.

---
+++ src/main/java/com/mycompany/icetask9/ICETASK9.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask9;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class ICETASK9 {
+
+    public static void main(String[] args) {
+        
+    }
+}
